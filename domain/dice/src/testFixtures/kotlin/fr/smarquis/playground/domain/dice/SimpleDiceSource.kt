f6d41e375bd0b1315ca27b5bc2637dc5781b8da5
Simon Marquis <contact@simon-marquis.fr>
2025-01-25 20:48:24 +0000

    🧪 Add missing ViewModel tests using domain testFixtures (#17)

--- domain/dice/src/testFixtures/kotlin/fr/smarquis/playground/domain/dice/SimpleDiceSource.kt
+++ domain/dice/src/testFixtures/kotlin/fr/smarquis/playground/domain/dice/SimpleDiceSource.kt
@@ -1,12 +1,13 @@
 package fr.smarquis.playground.domain.dice
 
+import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.plus
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
-public class SimpleDiceSource : DiceSource {
-    private val mutableRolls = MutableStateFlow(persistentListOf<Dice>())
+public class SimpleDiceSource(rolls: PersistentList<Dice>) : DiceSource {
+    private val mutableRolls = MutableStateFlow(rolls)
     override val rolls = mutableRolls
     override suspend fun roll(dice: Dice) = mutableRolls.update { it + dice }
     override suspend fun reset(): Unit = mutableRolls.update { persistentListOf() }
