84f5f38500861db5550c8d8c1c2abb66f5adf853
Simon Marquis <contact@simon-marquis.fr>
2025-01-26 14:36:26 +0000

    🗡️ Configure Dagger compiler options in the PlaygroundHiltPlugin (#28)

--- build-logic/src/main/kotlin/fr/smarquis/playground/buildlogic/PlaygroundKspPlugin.kt
+++ build-logic/src/main/kotlin/fr/smarquis/playground/buildlogic/PlaygroundKspPlugin.kt
@@ -1,19 +1,13 @@
 package fr.smarquis.playground.buildlogic
 
-import com.google.devtools.ksp.gradle.KspExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
 
 internal class PlaygroundKspPlugin : Plugin<Project> {
 
     override fun apply(target: Project) = with(target) {
         apply(plugin = "com.google.devtools.ksp")
-
-        extensions.configure<KspExtension> {
-            arg("dagger.hilt.disableModulesHaveInstallInCheck", "true")
-        }
     }
 
 }
