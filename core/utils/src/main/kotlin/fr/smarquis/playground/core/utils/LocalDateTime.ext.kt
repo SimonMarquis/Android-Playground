c915fd31abeee3761aa2f129afc189642f371442
Simon Marquis <contact@simon-marquis.fr>
2025-06-24 20:59:02 +0100

    Fix deprecation

--- core/utils/src/main/kotlin/fr/smarquis/playground/core/utils/LocalDateTime.ext.kt
+++ core/utils/src/main/kotlin/fr/smarquis/playground/core/utils/LocalDateTime.ext.kt
@@ -1,6 +1,6 @@
 package fr.smarquis.playground.core.utils
 
-import kotlinx.datetime.Instant.Companion.fromEpochMilliseconds
+import kotlin.time.Instant.Companion.fromEpochMilliseconds
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone.Companion.currentSystemDefault
 import kotlinx.datetime.toLocalDateTime
