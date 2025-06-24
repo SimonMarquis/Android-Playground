534d5d74c525f77b4fd2d7ec5a8941ef9bfed820
Simon Marquis <contact@simon-marquis.fr>
2025-06-24 21:11:27 +0100

    !fixup

--- feature/home/src/test/kotlin/fr/smarquis/playground/feature/home/HomeViewModelTest.kt
+++ feature/home/src/test/kotlin/fr/smarquis/playground/feature/home/HomeViewModelTest.kt
@@ -21,12 +21,13 @@ import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
-import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import org.junit.Rule
 import kotlin.test.Test
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class HomeViewModelTest {
