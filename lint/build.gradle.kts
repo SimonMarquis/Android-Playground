bc7bb12c983b7d967dd00394a0c1cc611de1c1fb
Simon Marquis <contact@simon-marquis.fr>
2025-09-28 15:56:51 +0200

    🧪 Force resolution of kotlin-test-junit (#370)

--- lint/build.gradle.kts
+++ lint/build.gradle.kts
@@ -12,7 +12,6 @@ dependencies {
     compileOnly(libs.kotlin.stdlib)
     compileOnly(libs.lint.api)
 
-    testImplementation(libs.kotlin.test)
     testImplementation(libs.lint.checks)
     testImplementation(libs.lint.tests)
 }
