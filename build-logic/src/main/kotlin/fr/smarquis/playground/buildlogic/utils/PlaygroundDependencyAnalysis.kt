7bf6ae1fff9eb89dee41a5e769f04dacd427535a
Simon Marquis <contact@simon-marquis.fr>
2026-04-25 12:04:55 +0200

    Move ABI exclusion to the root DependencyAnalysisExtension

--- build-logic/src/main/kotlin/fr/smarquis/playground/buildlogic/utils/PlaygroundDependencyAnalysis.kt
+++ build-logic/src/main/kotlin/fr/smarquis/playground/buildlogic/utils/PlaygroundDependencyAnalysis.kt
@@ -13,6 +13,11 @@ internal object PlaygroundDependencyAnalysis {
         apply<DependencyAnalysisPlugin>()
         extensions.configure<DependencyAnalysisExtension> {
             useTypesafeProjectAccessors(true)
+            abi {
+                exclusions {
+                    ignoreGeneratedCode()
+                }
+            }
             usage {
                 analysis {
                     checkSuperClasses(true)
