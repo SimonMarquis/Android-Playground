15927861a648e33ee2532ff4d5d2cad72a57d6df
Simon Marquis <contact@simon-marquis.fr>
2026-05-17 19:19:05 +0200

    Remove unnecessary binding

--- core/android/src/main/kotlin/fr/smarquis/playground/core/android/di/PlaygroundViewModelFactory.kt
+++ core/android/src/main/kotlin/fr/smarquis/playground/core/android/di/PlaygroundViewModelFactory.kt
@@ -1,12 +1,10 @@
 package fr.smarquis.playground.core.android.di
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesBinding
 import dev.zacsweers.metro.DelicateMetroApi
 import dev.zacsweers.metro.ExperimentalMetroApi
-import dev.zacsweers.metro.binding
 import dev.zacsweers.metrox.viewmodel.ManualViewModelAssistedFactory
 import dev.zacsweers.metrox.viewmodel.MetroViewModelFactory
 import dev.zacsweers.metrox.viewmodel.ViewModelAssistedFactory
@@ -14,7 +12,6 @@ import kotlin.reflect.KClass
 
 @OptIn(DelicateMetroApi::class, ExperimentalMetroApi::class)
 @ContributesBinding(AppScope::class)
-@ContributesBinding(AppScope::class, binding<ViewModelProvider.Factory>())
 public class PlaygroundViewModelFactory(
     override val viewModelProviders: Map<KClass<out ViewModel>, () -> ViewModel>,
     override val assistedFactoryProviders: Map<KClass<out ViewModel>, () -> ViewModelAssistedFactory>,
