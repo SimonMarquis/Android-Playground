fb308b10b4ea1d798159d87ec9f65b0b3144bbdd
Simon Marquis <contact@simon-marquis.fr>
2026-06-13 08:51:55 +0200

    🆕 Migrate to Explicit backing fields (#649)

--- domain/dice/src/testFixtures/kotlin/fr/smarquis/playground/domain/dice/SimpleDiceSource.kt
+++ domain/dice/src/testFixtures/kotlin/fr/smarquis/playground/domain/dice/SimpleDiceSource.kt
@@ -1,14 +1,17 @@
 package fr.smarquis.playground.domain.dice
 
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.plus
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 
 public class SimpleDiceSource(rolls: PersistentList<Dice>) : DiceSource {
-    private val mutableRolls = MutableStateFlow(rolls)
-    override val rolls = mutableRolls
-    override suspend fun roll(dice: Dice) = mutableRolls.update { it + dice }
-    override suspend fun reset(): Unit = mutableRolls.update { persistentListOf() }
+    override val rolls: StateFlow<ImmutableList<Dice>>
+        field = MutableStateFlow(rolls)
+
+    override suspend fun roll(dice: Dice) = rolls.update { it + dice }
+    override suspend fun reset(): Unit = rolls.update { persistentListOf() }
 }
